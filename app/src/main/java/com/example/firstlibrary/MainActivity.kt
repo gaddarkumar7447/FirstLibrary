8c31bf31ea5a4c0bcc26df4a668bd9bb3e677bcd
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2023-03-20 18:07:39 +0530

    Check my library working or not

--- app/src/main/java/com/example/firstlibrary/MainActivity.kt
+++ app/src/main/java/com/example/firstlibrary/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.firstlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.DesignTool
+import com.example.firstlibrarycreated.DesignToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DesignToast.smartToast(this, "This is main mainActivity")
+
     }
 }
